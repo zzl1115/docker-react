fe0ae72639db49886a77493a0e08939f7ac76cfc
Zhilong Zheng <zheng.zhil@husky.neu.edu>
2019-12-20 20:24:48 -0800

    added travis file

--- src/App.test.js
+++ src/App.test.js
@@ -8,9 +8,3 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
